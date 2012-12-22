dd7c55f4455c6f7dead49cc0f09de2430d915422
Mark Koberlein <mkoberlein@gmail.com>
2012-12-22 13:42:10 -0500

    first commit

---
+++ public/index.php
@@ -0,0 +1,115 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>LV Tech Bootstrapped</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link href="assets/vendors/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/vendors/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="assets/stylesheets/application.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+    <link rel="shortcut icon" href="../assets/ico/favicon.png">
+  </head>
+
+  <body>
+
+    <!-- NAVBAR
+    ================================================== -->
+    <div class="navbar-wrapper">
+      <!-- Wrap the .navbar in .container to center it within the absolutely positioned parent. -->
+      <div class="container">
+
+        <div class="navbar">
+          <div class="navbar-inner">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </a>
+            <a class="brand" href="#">Project name</a>
+            <div class="nav-collapse collapse">
+              <ul class="nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="http://meetup.lehighvalleytech.org">Events</a></li>
+                <li><a href="http://www.soundcloud.com/lvtech">Radio Show</a></li>
+                <li><a href="http://www.twitter.com/lvtech">Twitter</a></li>
+                <li><a href="http://www.youtube.com/user/lehighvalleytech">YouTube</a></li>
+                <!--
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li class="nav-header">Nav header</li>
+                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>
+                -->
+              </ul>
+            </div><!--/.nav-collapse -->
+          </div><!-- /.navbar-inner -->
+        </div><!-- /.navbar -->
+
+      </div> <!-- /.container -->
+    </div><!-- /.navbar-wrapper -->
+
+    <div id="featuredCarousel" class="carousel slide">
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="/assets/images/4119-christmas-background-photos.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Lehigh Valley Tech Christmas Meetup 2012</h1>
+              <p class="lead">The end of the year is upon us and it's time to celebrate the past year's accomplishments and look forward to what's coming up in the new year.</p>
+              <a class="btn btn-large btn-success" href="http://meetup.lehighvalleytech.org/events/74003572/">RSVP Today on Meetup.com</a>
+            </div>
+          </div>
+        </div>
+      </div>
+<!--  <a class="left carousel-control" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next">&rsaquo;</a>
+ -->    
+    </div><!-- /.carousel -->
+
+    <div class="container marketing">
+
+      <div class="featurette">
+        <h2 class="featurette-heading">About Our Next Meetup.</h2>
+        <p class="lead">
+<p><em><strong><span style="color : #ff0000">L</span><span style="color : #008000">e</span><span style="color : #ff0000">h</span><span style="color : #008000">i</span><span style="color : #ff0000">g</span><span style="color : #008000">h</span> <span style="color : #ff0000">V</span><span style="color : #008000">a</span><span style="color : #ff0000">l</span><span style="color : #008000">l</span><span style="color : #ff0000">e</span><span style="color : #008000">y</span> <span style="color : #ff0000">T</span><span style="color : #008000">e</span><span style="color : #ff0000">c</span><span style="color : #008000">h</span> <span style="color : #ff0000">C</span><span style="color : #008000">h</span><span style="color : #ff0000">r</span><span style="color : #008000">i</span><span style="color : #ff0000">s</span><span style="color : #008000">t</span><span style="color : #ff0000">m</span><span style="color : #008000">a</span><span style="color : #ff0000">s</span> <span style="color : #008000">P</span><span style="color : #ff0000">a</span><span style="color : #008000">r</span><span style="color : #ff0000">t</span><span style="color : #008000">y</span></strong></em></p> <p>The end of the year is upon us and it's time to celebrate the past year's accomplishments and look forward to what's coming up in the new year.</p> <p>To keep things fun and interesting, we will do a series of Lightning Talks for the presentation session. What does that mean? There will be some quick 5-minutes (no Q&amp;A) presentations on a tech or entrepreneurship topic.</p> <p>Here's the current line-up (order subject to change):</p> <ol> <li><strong>Building the Maker Community <br /> </strong> <em>Jared Steckel, <a href="http://makelehighvalley.com">Make Lehigh Valley</a></em></li> <li><strong>Professional Writing in Website Development <br /> </strong> <em>Joe McDermott, <a href="http://www.writerjoe.com/">McDermott Communications</a></em></li> <li><strong>New project: Startup Cell <br /> </strong> <em>Ryan Critchett, <a href="http://imobilerescue.com">iMobileRescue</a></em></li> <li><strong>Benefits of Coworking <br /> </strong> Santiago Rivera, <a href="http://sobecoworks.com">SoBeCoWorks</a></li> <li><strong>Technology in Education <br /> </strong> Rita Chuhran, <a href="http://www.skaffl.com">Skaffl</a></li> <li><strong>Barlinq: Startup Weekend Update From Bar-o-Metrics <br /> </strong> <span>Henry Ancher,</span> <a href="http://bar-o-metrics.com">Bar-o-Metrics</a></li> </ol> <p>Additionally, Lehigh Valley Tech Radio will broadcast the evening LIVE on the Internet (assuming we can figure out all of the tech stuff between now and then &lt;fingers crossed&gt;).</p> <p>We will have treats and beverages available for one and all (feel free to bring some homemade treats to share). We will also fill you in on some of the things we have planned for 2013 and we will ask you to weigh in on what events that you'd like to see happen.</p>
+        </p>
+      </div>
+
+      <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2012 Lehigh Valley Tech. &middot;</p>
+      </footer>
+
+    </div><!-- /.container -->
+
+    <script src="assets/vendors/jquery/jquery-1.8.3.js"></script>
+    <script src="assets/vendors/bootstrap/bootstrap.js"></script>
+    <script>
+      !function ($) {
+        $(function(){
+          $('#featuredCarousel').carousel()
+        })
+      }(window.jQuery)
+    </script>
+  </body>
+</html>
